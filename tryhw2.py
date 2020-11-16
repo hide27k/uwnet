13b189fa779717cc39a1b2236d619370246de3dd
hide27k <hidehusky1996@gmail.com>
2020-11-16 12:31:37 -0800

    Add the updated model into tryhw2.py

--- tryhw2.py
+++ tryhw2.py
@@ -12,6 +12,22 @@ def conv_net():
             make_activation_layer(SOFTMAX)]
     return make_net(l)
 
+def conv_net_with_batch():
+    l = [   make_convolutional_layer(32, 32, 3, 8, 3, 2),
+            make_batchnorm_layer(8),
+            make_activation_layer(RELU),
+            make_maxpool_layer(16, 16, 8, 3, 2),
+            make_convolutional_layer(8, 8, 8, 16, 3, 1),
+            make_batchnorm_layer(16),
+            make_activation_layer(RELU),
+            make_maxpool_layer(8, 8, 16, 3, 2),
+            make_convolutional_layer(4, 4, 16, 32, 3, 1),
+            make_batchnorm_layer(32),
+            make_activation_layer(RELU),
+            make_connected_layer(512, 10),
+            make_activation_layer(SOFTMAX)]
+    return make_net(l)
+
 
 print("loading data...")
 train = load_image_classification_data("cifar/cifar.train", "cifar/cifar.labels")
@@ -38,3 +54,7 @@ print("test accuracy:     %f", accuracy_net(m, test))
 
 # 7.6 Question: What do you notice about training the convnet with/without batch normalization? How does it affect convergence? How does it affect what magnitude of learning rate you can use? Write down any observations from your experiments:
 # TODO: Your answer
+# 
+
+# The default model returns a training accuracy of 0.405460000038147 and a test accuract of 0.4027000069618225.
+
